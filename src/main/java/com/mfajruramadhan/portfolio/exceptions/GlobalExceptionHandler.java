70a0eae753768ce100f06f7ce69c8334f6590910
MFajru <fajru234@gmail.com>
2025-02-16 15:26:50 +0700

    feat: error handling

--- src/main/java/com/mfajruramadhan/portfolio/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/mfajruramadhan/portfolio/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,17 @@
 package com.mfajruramadhan.portfolio.exceptions;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.mfajruramadhan.portfolio.dto.Response;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.web.server.WebServer;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,6 +28,34 @@ public class GlobalExceptionHandler {
         return res;
     }
 
-    // change default bad request response
-    @ExceptionHandler
+    @ExceptionHandler({BadRequestException.class, MethodArgumentTypeMismatchException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response<RuntimeException> handleBadRequestException(RuntimeException exception) {
+
+        logger.warn(exception.getMessage());
+
+        Response<RuntimeException> res = new Response<>();
+        res.setData(null);
+        res.setMessage(exception.getMessage());
+        return res;
+
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response<HttpMessageNotReadableException> handleInvalidRequestValueTypeException(HttpMessageNotReadableException exception) {
+        logger.warn(exception.getMessage());
+
+        String fName = "";
+        String expectedVal = "";
+        if (exception.getCause() instanceof InvalidFormatException e) {
+            fName = e.getPath().get(0).getFieldName();
+            expectedVal = e.getTargetType().getSimpleName();
+        }
+        Response<HttpMessageNotReadableException> res = new Response<>();
+        res.setData(null);
+        res.setMessage(String.format("%s expected value of %s in field %s", exception.getMessage().substring(0,17), expectedVal, fName));
+        return res;
+
+    }
 }
